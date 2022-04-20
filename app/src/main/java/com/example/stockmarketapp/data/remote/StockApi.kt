4537d9a79e6092477947c559ce49edf24b2ac17e
João Alves <alvezsjv@gmail.com>
2022-04-20 16:08:46 -0300

    fix(StockApi): change invalid configs

--- app/src/main/java/com/example/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/example/stockmarketapp/data/remote/StockApi.kt
@@ -6,13 +6,13 @@ import retrofit2.http.Query
 
 interface StockApi {
 
-    @GET("query/function=LISTING_STATUS")
+    @GET("query?function=LISTING_STATUS")
     suspend fun getListings(
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
 
     companion object {
-        const val API_KEY = "G#asdasd"
+        const val API_KEY = "G1USXWKX272RK4BP"
         const val BASE_URL = "https://alphavantage.co"
     }
 }
